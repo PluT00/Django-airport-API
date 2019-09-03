39dc89d392be7db66445d91388a326843ae301da
PluT00 <valeran2003@mail.ru>
2019-09-03 22:15:15 +0300

    Get SECRET_KEY from env var

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v89%=&xff6@wb-ak329w9+_g7g+-9k9#z(k0oe+mi1aed!#&%z'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
