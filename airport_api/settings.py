403c4e6558e8103591a244058e23554513c5e307
PluT00 <valeran2003@mail.ru>
2019-09-08 15:06:34 +0300

    Change database

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'airport_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'airportapi',
-        'USER': 'airportapiuser',
-        'PASSWORD': '111',
+        'NAME': 'djairport',
+        'USER': 'djairportuser',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
