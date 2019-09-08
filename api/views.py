172ac7d2d5d1b2d2a8ef944a4f96b4537cea0626
PluT00 <valeran2003@mail.ru>
2019-09-08 15:07:50 +0300

    Add views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from api.models import Flight, Plane, Ticket
+from api.serializers import FlightSerializer, PlaneSerializer, TicketSerializer, UserSerializer
 
-# Create your views here.
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class PlaneViewSet(viewsets.ModelViewSet):
+    queryset = Plane.objects.all()
+    serializer_class = PlaneSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
