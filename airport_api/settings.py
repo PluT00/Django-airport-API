8b594dcdda52a610d46aae53831db60b1c261250
PluT00 <valeran2003@mail.ru>
2019-09-10 03:01:07 +0300

    Add django_heroku settings

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,9 +79,9 @@ WSGI_APPLICATION = 'airport_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djairport',
-        'USER': 'djairportuser',
-        'PASSWORD': '',
+        'NAME': 'airportapi',
+        'USER': 'airportapiuser',
+        'PASSWORD': '111',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
@@ -124,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
